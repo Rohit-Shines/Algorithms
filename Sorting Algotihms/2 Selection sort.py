9f6cc3615146a566ec289a98606c91946d5b520f
rohit-shines <rohityahoooo@gmail.com>
2022-10-17 15:34:33 -0300

    .

--- Sorting Algotihms/2 Selection sort.py
+++ Sorting Algotihms/2 Selection sort.py
@@ -1,14 +1,14 @@
 # this will move minimum number to 1st index
 # second minimum number to 2nd index
 
-def selection_sort(arr=[6,4,0,1,3]):
-    for i in range(len(arr)): # 0 - 4
+def selection_sort(nums=[6, 4, 0, 1, 3]):
+    for i in range(len(nums)): # 0 - 4
         minimum = i
-        for j in range(i + 1, len(arr)): # 1-4 / 2-4 / 3-4 // leaving first index
-            if arr[j] < arr[minimum]: # 4<6
+        for j in range(i + 1, len(nums)): # 1-4 / 2-4 / 3-4 // leaving first index
+            if nums[j] < nums[minimum]: # 4<6
                 minimum = j
                 print("test")
-        arr[minimum], arr[i] = arr[i], arr[minimum] # # Place it at the front of the
+        nums[minimum], nums[i] = nums[i], nums[minimum] # # Place it at the front of the
 
-        print(arr)
+        print(nums)
 selection_sort()
